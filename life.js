7993057d072d7f33955db3e419ce0971921fa165
es <129210679+cool-es@users.noreply.github.com>
2024-02-12 15:59:55 +0100

    JS: Add rudimentary universe operations

--- life.js
+++ life.js
@@ -11,6 +11,14 @@ const functionImports = { env: {} };
 function main(result) {
     console.log('WASM loaded!')
     window.rustwasm = result.instance.exports;
+    const uni = rustwasm.allocateUniverse();
+    const length = 32**2;
+
+    window.universe = uni;
+    window.tick = () => { rustwasm.tickUniverse(uni); }
+    window.view = () => {
+        return new Uint8Array(rustwasm.memory.buffer, uni).subarray(0, length);
+    }
 }
 
 function failure(error) { console.error(error); }
